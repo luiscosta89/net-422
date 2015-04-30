3d923b55a7038c8c2d5dbd9873b971c7fdb05fb1
Atilla Gallio <atilla.gallio@gmail.com>
2015-04-30 12:49:00 -0500

    Debugging insert.php

--- php/insert.php
+++ php/insert.php
@@ -7,13 +7,12 @@ include("../sql/connect.php");
      
 //attempt insert query execution
 $sql = "INSERT INTO ideas (name, brief, descrption, category, image_1, image_2, image_3) VALUES ('".$_POST[ideaName]."', '".$_POST[brief]."', '".$_POST[description]."', '".$_POST[category]."', '".$_POST[img1]."', '".$_POST[img2]."', '".$_POST[img3]."')";
-
+mysqli_query($conn, $sql);
 $query = 'SELECT id FROM ideas ORDER BY id DESC LIMIT 1';
 $result = $conn->query($query);
 $row = $result->fetch_assoc(); 
-
 $sql2 = "INSERT INTO ideasgames (game_id, ideas_id) VALUES ('".$_POST[games]."', '".$row[id]."')";
-if(mysqli_query($conn, $sql) && mysqli_query($conn, $sql2)){
+if(mysqli_query($conn, $sql2)){
     echo "Records added successfully.";
      header( 'Location: ../index.php' ) ;
 } else{
