4082897c46f05696578803d0eb3eba0c0bfe9d44
Atilla Gallio <atilla.gallio@gmail.com>
2015-04-30 03:23:57 -0500

    index changes
    WTF

--- index.php
+++ index.php
@@ -11,35 +11,17 @@
 
 <div class="section">
     <div class="container">
+       
         <div class="row">
-            <!-- Featured News -->
-            <div class="col-sm-6 featured-news">
-                <h2>Latest News</h2>
 
+            <div class="col-sm-12">
+                    
+             </div>
 
-                <div class="row">
-                    <div class="col-xs-4"><a href="page-blog-post-right-sidebar.html"><img src="../img/news1.jpg" alt="Post Title"></a></div>
-                    <div class="col-xs-8">
-                        <div class="caption"><a href="page-blog-post-right-sidebar.html">WebSite Deployed</a></div>
-                        <div class="date">16 Dez 2014 </div>
-                        <div class="intro"> WebSite deployed to Azure. <a href="page-blog-post-right-sidebar.html"></a></div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Featured News -->
-            <!-- Latest News -->
-            <div class="col-sm-6 latest-news">
-                <h2>Updates</h2>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="caption"><a href="page-blog-post-right-sidebar.html">Test</a></div>
-                        <div class="date">16 Dez 2014 </div>
-                        <div class="intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. <a href="#">Read more...</a></div>
-                    </div>
-                </div>
-            </div>
-            <!-- End Featured News -->
         </div>
+
+        <?php include("php/ideaslist.php");?>
+
     </div>
 </div>
 
