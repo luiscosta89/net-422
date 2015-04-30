cab13bd1616c745f99a9549d677b997d3d60259d
Atilla Gallio <atilla.gallio@gmail.com>
2015-04-30 03:15:28 -0500

    Showing Ideas list
    Working but we still have to make it look good :dancer:

---
+++ php/ideaslist.php
@@ -0,0 +1,32 @@
+<?php
+
+ include("sql/connect.php");
+
+$query = "SELECT * FROM ideas";
+
+
+ $result =$conn->query($query); //you can also use here or die(mysql_error());
+
+  if($result){
+         while($row= $result->fetch_assoc()) {
+  
+   
+    echo"<div class='row'>";
+    echo"<div class='col-sm-12'>";
+    echo"<div class='portfolio-item'>";
+    echo"<ol class='breadcrumb bread-primary '>"; 
+
+    echo"<li><a href='#'>".$row['name'] ."</a></li>"; 
+
+    echo"</ol>"; 
+    echo"</div>";
+    echo"</div>";
+    echo"</div>"; 
+
+ 	} 
+ }
+
+
+$conn->close();
+
+?>
--- sql/connect.php
+++ sql/connect.php
@@ -4,22 +4,19 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "games_data";
+$servername2 = "50.62.209.37:3306";
 $username2 = "atilla";
 $password2 = "atilla12345";
 
 // Create connection
-$conn = new mysqli($servername, $username2, $password2, $dbname);
+$conn = new mysqli($servername2, $username2, $password2, $dbname);
 // Check connection
 if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 }
 else
 {
-	echo "Success!";
+	$connected = 1;
 }
 
-$conn->close();
 ?>  
-
-</body>
-</html>
