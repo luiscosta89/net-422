aa8d9423f4d7d4a654318bd7db630bc8769b198d
Atilla Gallio <atilla.gallio@gmail.com>
2015-04-30 01:19:22 -0500

    sql connect testing
    just for easy testing

--- sql/connect.php
+++ sql/connect.php
@@ -4,9 +4,11 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "games_data";
+$username2 = "atilla";
+$password2 = "atilla12345"
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username2, $password2, $dbname);
 // Check connection
 if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
