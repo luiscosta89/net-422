9b579501c12e745d85b303dda33eabff46b73384
Luis Antonio Costa <luis.leite89@gmail.com>
2015-04-30 05:01:46 -0500

    Add new idea

---
+++ new_idea.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+<?php include("include/header.php");?>
+<body>
+    
+<!-- Header -->
+
+<?php include("include/logo.php");?>
+
+
+    <!-- Page Title -->
+		<div class="section section-breadcrumbs">
+			<div class="container">
+				<div class="row">
+				<div id="errorSpace"> </div>
+					<div class="col-md-12">						
+						<h1>New Idea</h1>
+						<td>&nbsp;</td>
+							<form action="new_game.php" name="game" method="get">
+							<font color="orange">
+							Name: <input type="text" name="userName" size="30"><br>							
+							Brief explanation: <input type="text" name="brief" size="60"><br>							
+							Full description: <textarea name="description" cols="50" rows="5"></textarea><br>
+							Category: <input type="date" name="date"><br>
+							Image 1 Link: <input type="text" name="img1" size="50"><br>
+							Image 2 Link: <input type="text" name="img2" size="50"><br>
+							Image 3 Link: <input type="text" name="img3" size="50"><br>
+							<a href="#" class="btn btn"><i class="icon-shopping-cart icon-white"></i> Submit!</a>							
+							</font>
+							</form>
+	    			</div>
+	    			<!-- End Idea Summary & Options -->  			
+	    		</div>
+			</div>
+		</div>
+		
+	<script>
+
+//The method to validate form inputs
+function validate()
+{
+   // Validate name
+   name = document.game.userName.value;   
+   if (name == "")
+      {
+      displayError("You should provide a name!");
+      return false;  //there is an error
+      }   
+	  
+	// Validate brief
+   name = document.game.brief.value;   
+   if (name == "")
+      {
+      displayError("You should provide a brief explanation!");
+      return false;  //there is an error
+      }
+
+	// Validate description
+   name = document.game.type.value;   
+   if (name == "")
+      {
+      displayError("You should provide a full description!");
+      return false;  //there is an error
+      }
+	  
+	// Validate image links
+   test1 = document.game.img1.value;
+   test2 = document.game.img2.value;
+   test3 = document.game.img3.value;
+   if (test1 == "" && test2 == "" && test3 == "")
+      {
+      displayError("You should provide at least one image link!");
+      return false;  //there is an error
+      }
+	
+}
+
+</script>
+	
+	<!-- Footer -->
+    >
+<?php include("include/footer.php");?>
+
+</body>
+
+</html>
